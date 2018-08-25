e2fc3dbaef3b8aa314c8bd7faedc40589d21a95f
Gavin Baker <gavnbaker@gmail.com>
2018-08-25 13:20:59 -0400

    Added job tracker api to server

---
+++ server/job-tracker-api/src/main/java/io/springboot/jobtrackerapi/JobTrackerApiApplication.java
@@ -0,0 +1,12 @@
+package io.springboot.jobtrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobTrackerApiApplication.class, args);
+	}
+}
